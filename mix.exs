c311c41d3384ef14a1e3ffd292b5234c2ace7043
Brian Underwood <brian@brian-underwood.codes>
2021-09-19 13:40:52 +0200

    Metadata and ex_doc for publishing

--- mix.exs
+++ mix.exs
@@ -7,7 +7,17 @@ defmodule CredoTodoOrDie.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Exploding TODO/FIXME/etc... notes as a credo check",
+      package: package(),
+      source_url: "https://github.com/cheerfulstoic/credo_todo_or_die"
+    ]
+  end
+
+  def package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/cheerfulstoic/credo_todo_or_die"},
     ]
   end
 
@@ -24,7 +34,8 @@ defmodule CredoTodoOrDie.MixProject do
       {:credo, "~> 1.5", only: [:dev, :test]},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       {:timex, "~> 3.7"},
-      {:tzdata, "~> 1.1"}
+      {:tzdata, "~> 1.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
